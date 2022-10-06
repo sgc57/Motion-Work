6edfc5ef9bb4414d4a5bf275f3ef96ca81586786
sgc57 <sgc57@drexel.edu>
2022-10-06 00:50:33 -0400

    oh hell yeah

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,16 +10,16 @@ let rocketBtn = document.querySelector("#rocket-btn");
 rocketBtn.addEventListener("mouseover",function(){
     gsap.to("#rocket-btn",{duration:1, scale:2});
 
-    gsap.to("#rocket-btn i",{duration: 1, x: 10});
+    gsap.to("#rocket-btn i",{duration: 1, y: -50, alpha: 0});
 
-    gsap.to("#first-line",{duration: 1, alpha:0, x:75});
+    gsap.to("#first-line",{duration: 1, alpha:0, x:100});
     gsap.to("#second-line",{duration: 1, alpha:0, x:100});
 })
 
 rocketBtn.addEventListener("mouseout",function(){
     gsap.to("#rocket-btn",{duration:1, scale:1});
 
-    gsap.to("#rocket-btn i",{duration: 1, x: 0});
+    gsap.to("#rocket-btn i",{duration: 1, y: 0, alpha: 1});
 
     gsap.to("#first-line",{duration: 1, alpha:1, x:0});
     gsap.to("#second-line",{duration: 1, alpha:1, x:0});
