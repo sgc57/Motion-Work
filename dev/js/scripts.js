79ec39c944526cad4efa004beda8d380b55d4283
sgc57 <sgc57@drexel.edu>
2022-10-06 00:47:51 -0400

    it works!!!!

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,26 @@
 import { gsap } from "gsap";
 
-gsap.from(".first-line",{duration: 1, x: -100, alpha: 0});
+gsap.from("#first-line",{duration: 1, x: -100, alpha: 0});
 gsap.from("#second-line",{duration: 1, x: -75, alpha:0, delay: 0.5});
 gsap.from("#rocket-btn",{duration: 1, x: -50, alpha:0, delay: 0.75});
+gsap.from("#rocket-btn i",{duration: 1, y: 35, alpha:0, delay: 0.75});
+
+let rocketBtn = document.querySelector("#rocket-btn");
+
+rocketBtn.addEventListener("mouseover",function(){
+    gsap.to("#rocket-btn",{duration:1, scale:2});
+
+    gsap.to("#rocket-btn i",{duration: 1, x: 10});
+
+    gsap.to("#first-line",{duration: 1, alpha:0, x:75});
+    gsap.to("#second-line",{duration: 1, alpha:0, x:100});
+})
+
+rocketBtn.addEventListener("mouseout",function(){
+    gsap.to("#rocket-btn",{duration:1, scale:1});
+
+    gsap.to("#rocket-btn i",{duration: 1, x: 0});
+
+    gsap.to("#first-line",{duration: 1, alpha:1, x:0});
+    gsap.to("#second-line",{duration: 1, alpha:1, x:0});
+})
