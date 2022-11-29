9c3340a590ae0139aaf1080f4ff345f14297a0e1
sgc57 <sgc57@drexel.edu>
2022-11-29 00:28:11 -0500

    something i guess

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,6 +6,7 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 function textDraw(){
     var tl = gsap.timeline()
+    tl.fromTo("z-group, #i-group, #d-group, #y-group", {alpha:"0%", scale:"0%"}, {alpha:"100%", scale:"100%", duration:1, stagger:0.25},"textmove")
     tl.fromTo("#z-mask, #i-mask, #d-mask, #y-mask",{drawSVG:"0% 100%"}, {drawSVG:"100% 100%", duration:0.75, stagger:0.25},"textmove")
     tl.fromTo("#i-dot", {y:60, alpha:0}, {alpha:100, y:-30, duration:0.25})
     tl.to("#i-dot", {y:0, duration:0.25})
