e1a96a9098ed1d42234f3126b4d093bf4de87920
sgc57 <sgc57@drexel.edu>
2022-12-01 00:18:09 -0500

    pause

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,12 +1,18 @@
 import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+// import { MotionPathHelper } from "gsap/MotionPathHelper";
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools);
+
+function chatBubble(){
+
+}
 
 function textDraw(){
     var tl = gsap.timeline()
-    tl.fromTo("z-group, #i-group, #d-group, #y-group", {alpha:"0%", scale:"0%"}, {alpha:"100%", scale:"100%", duration:1, stagger:0.25},"textmove")
+    tl.fromTo("#z-group, #i-group, #d-group, #y-group", {alpha:"0%", scale:0}, {alpha:"100%", scale:1, duration:1, stagger:0.25},"textmove")
     tl.fromTo("#z-mask, #i-mask, #d-mask, #y-mask",{drawSVG:"0% 100%"}, {drawSVG:"100% 100%", duration:0.75, stagger:0.25},"textmove")
     tl.fromTo("#i-dot", {y:60, alpha:0}, {alpha:100, y:-30, duration:0.25})
     tl.to("#i-dot", {y:0, duration:0.25})
@@ -14,6 +20,8 @@ function textDraw(){
 }
 
 var mainTL = gsap.timeline();
+mainTL.add(chatBubble())
 mainTL.add(textDraw());
 
 GSDevTools.create();
+// MotionPathHelper.create();
